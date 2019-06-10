3e6d1ee4487788565a4d045cfe2b6a0f31982523
ILYA <ilya.aurov@gmail.com>
2019-06-10 12:24:45 +0300

    update>add simple test

--- Net640/apps/user_profile/tests.py
+++ Net640/apps/user_profile/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from Net640.apps.user_profile.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestUserPermission(TestCase):
+    def test_add_to_friends(self):
+        user1 = User(username='iam1', email='iam1@m.ru')
+        user1.save()
+
+        user2 = User(username='iam2', email='iam2@m.ru')
+        user2.save()
+
+        user1.accept(user2)
+        user2.accept(user1)
+
+        assert user1.get_friends()[0].username == 'iam2'
+        assert user2.get_friends()[0].username == 'iam1'
