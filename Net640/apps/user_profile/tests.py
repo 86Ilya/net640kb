b19e3ae5b8138e36d8aa7acd3cbe753588329b5a
Ilya <ilya.aurov@gmail.com>
2019-06-20 06:06:24 +0300

    update> profile tests was refactored

--- Net640/apps/user_profile/tests.py
+++ Net640/apps/user_profile/tests.py
@@ -1,3 +1,5 @@
+from uuid import uuid1
+
 from django.test import TestCase
 from Net640.apps.user_profile.models import User
 from Net640.apps.user_profile.models import RELATIONSHIP_FRIENDS, RELATIONSHIP_REQUEST_HAS_SENT
@@ -5,83 +7,60 @@ from Net640.apps.user_profile.models import RELATIONSHIP_WAITING_FOR_ACCEPT, NO_
 
 
 class TestUserPermission(TestCase):
+    def setUp(self):
+        random_name = str(uuid1())
+        self.user1 = User(username=random_name, email=random_name + '@m.ru')
+        self.user1.set_password('12345678')
+        self.user1.save()
+
+        random_name = str(uuid1())
+        self.user2 = User(username=random_name, email=random_name + '@m.ru')
+        self.user2.set_password('12345678')
+        self.user2.save()
+
     def test_send_request_for_relationship(self):
-        user1 = User(username='test_send_request_for_relationship1', email='test_send_request_for_relationship1@m.ru')
-        user1.save()
+        self.user1.accept(self.user2)
 
-        user2 = User(username='test_send_request_for_relationship2', email='test_send_request_for_relationship2@m.ru')
-        user2.save()
-
-        user1.accept(user2)
-
-        assert user1.check_relationship(user2) == RELATIONSHIP_REQUEST_HAS_SENT
-        assert user2.check_relationship(user1) == RELATIONSHIP_WAITING_FOR_ACCEPT
+        self.assertEqual(self.user1.check_relationship(self.user2), RELATIONSHIP_REQUEST_HAS_SENT)
+        self.assertEqual(self.user2.check_relationship(self.user1), RELATIONSHIP_WAITING_FOR_ACCEPT)
 
     def test_cancel_own_send_request_for_relationship(self):
-        user1 = User(username='test_cancel_send_request_for_relationship1',
-                     email='test_cancel_send_request_for_relationship1@m.ru')
-        user1.save()
+        self.user1.accept(self.user2)
 
-        user2 = User(username='test_cancel_send_request_for_relationship2',
-                     email='test_cancel_send_request_for_relationship2@m.ru')
-        user2.save()
+        self.assertEqual(self.user1.check_relationship(self.user2), RELATIONSHIP_REQUEST_HAS_SENT)
+        self.assertEqual(self.user2.check_relationship(self.user1), RELATIONSHIP_WAITING_FOR_ACCEPT)
 
-        user1.accept(user2)
+        self.user1.cancel(self.user2)
 
-        assert user1.check_relationship(user2) == RELATIONSHIP_REQUEST_HAS_SENT
-        assert user2.check_relationship(user1) == RELATIONSHIP_WAITING_FOR_ACCEPT
-
-        user1.cancel(user2)
-
-        assert user1.check_relationship(user2) == NO_RELATIONSHIP
-        assert user2.check_relationship(user1) == NO_RELATIONSHIP
+        self.assertEqual(self.user1.check_relationship(self.user2), NO_RELATIONSHIP)
+        self.assertEqual(self.user2.check_relationship(self.user1), NO_RELATIONSHIP)
 
     def test_cancel_foreign_send_request_for_relationship(self):
-        user1 = User(username='test_cancel_foreign_send_request_for_relationship1',
-                     email='test_cancel_foreign_send_request_for_relationship1@m.ru')
-        user1.save()
+        self.user1.accept(self.user2)
 
-        user2 = User(username='test_cancel_foreign_send_request_for_relationship2',
-                     email='test_cancel_foreign_send_request_for_relationship2@m.ru')
-        user2.save()
+        self.assertEqual(self.user1.check_relationship(self.user2), RELATIONSHIP_REQUEST_HAS_SENT)
+        self.assertEqual(self.user2.check_relationship(self.user1), RELATIONSHIP_WAITING_FOR_ACCEPT)
 
-        user1.accept(user2)
+        self.user2.cancel(self.user1)
 
-        assert user1.check_relationship(user2) == RELATIONSHIP_REQUEST_HAS_SENT
-        assert user2.check_relationship(user1) == RELATIONSHIP_WAITING_FOR_ACCEPT
-
-        user2.cancel(user1)
-
-        assert user1.check_relationship(user2) == NO_RELATIONSHIP
-        assert user2.check_relationship(user1) == NO_RELATIONSHIP
+        self.assertEqual(self.user1.check_relationship(self.user2), NO_RELATIONSHIP)
+        self.assertEqual(self.user2.check_relationship(self.user1), NO_RELATIONSHIP)
 
     def test_add_to_friends(self):
-        user1 = User(username='test_add_to_friends1', email='test_add_to_friends1@m.ru')
-        user1.save()
+        self.user1.accept(self.user2)
+        self.user2.accept(self.user1)
 
-        user2 = User(username='test_add_to_friends2', email='test_add_to_friends2@m.ru')
-        user2.save()
+        self.assertEqual(self.user1.check_relationship(self.user2), RELATIONSHIP_FRIENDS)
+        self.assertEqual(self.user2.check_relationship(self.user1), RELATIONSHIP_FRIENDS)
 
-        user1.accept(user2)
-        user2.accept(user1)
-
-        assert user1.check_relationship(user2) == RELATIONSHIP_FRIENDS
-        assert user2.check_relationship(user1) == RELATIONSHIP_FRIENDS
-
-        assert user1.get_friends()[0].username == 'test_add_to_friends2'
-        assert user2.get_friends()[0].username == 'test_add_to_friends1'
+        self.assertEqual(self.user1.get_friends()[0].username, self.user2.username)
+        self.assertEqual(self.user2.get_friends()[0].username, self.user1.username)
 
     def test_remove_from_friends(self):
-        user1 = User(username='test_remove_from_friends1', email='test_remove_from_friends1@m.ru')
-        user1.save()
+        self.user1.accept(self.user2)
+        self.user2.accept(self.user1)
 
-        user2 = User(username='test_remove_from_friends2', email='test_remove_from_friends2@m.ru')
-        user2.save()
+        self.user1.cancel(self.user2)
 
-        user1.accept(user2)
-        user2.accept(user1)
-
-        user1.cancel(user2)
-
-        assert user1.check_relationship(user2) == NO_RELATIONSHIP
-        assert user2.check_relationship(user1) == NO_RELATIONSHIP
+        self.assertEqual(self.user1.check_relationship(self.user2), NO_RELATIONSHIP)
+        self.assertEqual(self.user2.check_relationship(self.user1), NO_RELATIONSHIP)
