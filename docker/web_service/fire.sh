4bff51fda2c43c98525967d0cc4535e389dc2f79
Ilya <ilya.aurov@gmail.com>
2020-01-08 07:18:57 +0300

    update> migrations are back update> development configs

--- docker/web_service/fire.sh
+++ docker/web_service/fire.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-python /app/manage.py makemigrations 
-python /app/manage.py migrate 
+if [[ "$DEPLOY_ENV" -eq "DEVELOP" ]]
+then
+  python /app/manage.py makemigrations
+fi
+python /app/manage.py migrate
 python /app/manage.py collectstatic --noinput
 echo "launching uwsgi"
-uwsgi --ini /app/docker/web_service/uwsgi.ini 
+uwsgi --ini /app/docker/web_service/uwsgi.ini
 echo "launching daphne server"
 daphne -p 8001 -b 0.0.0.0 --access-log /app/logs/daphne_access.log Net640.asgi:application
